9580569bb1bcf8b154e9133cfe87ac74804d9f87
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2023-04-02 09:22:40 -0400

    Co-authored-by: plu0907 <plu0907@users.noreply.github.com>

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	plugins: [require("prettier-plugin-tailwindcss")],
+	arrowParens: "always",
+	singleQuote: false,
+	jsxSingleQuote: false,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	bracketSpacing: true,
+	bracketSameLine: true,
+	trailingComma: "all",
+};
