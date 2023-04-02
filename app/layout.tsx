31d90e2623666f9e5488dce178ce36f71dc1401e
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2023-04-02 09:48:57 -0400

    add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
-import './globals.css'
-
+import "./globals.css";
 
 export const metadata = {
-  title: 'HACK BUA',
-  description: 'Hackathon project for BUA by Ben Feuer, Patrick Lu, and Tem Taepaisitphongse',
-}
+	title: "HACK BUA",
+	description:
+		"Hackathon project for BUA by Ben Feuer, Patrick Lu, and Tem Taepaisitphongse",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className="bg-white text-black dark:bg-stone-950 dark:text-white">
+				{children}
+			</body>
+		</html>
+	);
 }
